d74292025ec67aff6e801f6b711ac3072b47d1d4
mp-martin <martin.paper@gmail.com>
2023-04-22 11:17:23 +0200

    Logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,21 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        headhunter: {
+          primary: "#E02735",
+          neutral: "#292A2B",
+          "neutral-content": "#7E7E7E",
+          "base-100": "#222224",
 
+          "--rounded-box": "0", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "0", // border radius rounded-badge utility class, used in badges and similar
+        },
+      },
+    ],
+  },
+}
