45aa75b6e3edaf5401ee031479ad3aaf61990781
Nieopodal <nieopodal.dev@gmail.com>
2023-04-22 15:09:06 +0200

    Created UserContext and necessary temporary interfaces.

---
+++ src/contexts/user.context.ts
@@ -0,0 +1,31 @@
+import {createContext} from "react";
+
+enum TemporaryUserRole {
+    Admin,
+    Student,
+    Hr,
+}
+
+interface TemporaryUserEntity {
+    id: string;
+    name: string;
+    surname: string;
+    email: string;
+    role: TemporaryUserRole;
+}
+
+interface UserContextInterface {
+    user: TemporaryUserEntity | null;
+    isLoading: boolean;
+    setUser: (payload: TemporaryUserEntity | null) => void;
+    error: string | null;
+}
+
+// @TODO :delete temporary interfaces and get the real ones
+
+export const UserContext = createContext<UserContextInterface>({
+    user: null,
+    error: null,
+    isLoading: false,
+    setUser: () =>{},
+});
