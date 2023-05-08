5c3241d46506a41bfac5f45c54bbd74cc0968d51
Nieopodal <nieopodal.dev@gmail.com>
2023-05-08 20:35:57 +0200

    Created basic Admin Dashboard view.

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -3,6 +3,7 @@ import {UserContext} from "../contexts/user.context";
 import {StudentDashboardView} from "./StudentDashboardView";
 import {HrView} from "./HrView/HrView";
 import {UserRole} from "../types/UserRole";
+import {AdminView} from "./AdminView";
 
 export const DashboardView = () => {
     const {user} = useContext(UserContext);
@@ -10,5 +11,6 @@ export const DashboardView = () => {
     return <div>
         {user!.role === UserRole.Student && <StudentDashboardView/>}
         {user!.role === UserRole.Hr && <HrView/>}
+        {user!.role === UserRole.Admin && <AdminView/>}
         </div>
 };
