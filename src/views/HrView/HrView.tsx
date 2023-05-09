2e2bb4db5de1a075374cc9635cd19140ffef94aa
mp-martin <martin.paper@gmail.com>
2023-05-09 22:27:57 +0200

    render logic implemented

--- src/views/HrView/HrView.tsx
+++ src/views/HrView/HrView.tsx
@@ -1,31 +1,32 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useCallback, useContext, useEffect, useRef, useState} from "react";
 import {BiSearch} from "react-icons/bi";
 import {FaFilter} from "react-icons/fa";
-import {SampleStudent, sampleStudents} from "../../helpers/sampleStudents";
-import {SingleStudent} from "./SingleStudent";
+// import {SampleStudent, sampleStudents} from "../../helpers/sampleStudents";
 import {HrViewMode} from "../../types/HrViewMode"
 import {HrTab} from "./HrTab";
 import {HrPagination} from "./HrPagination";
-import { useModal } from '../../hooks/useModal'
+import {useModal} from '../../hooks/useModal'
 import {FilteringModal} from "./FilteringModal";
 import {HrFilteringCriteria} from "../../types/HrFilteringCriteria";
 import {useFetch} from "../../hooks/useFetch";
 import {UserContext} from "../../contexts/user.context";
 import {AvailableStudentsResponse} from "../../types/AvailableStudentsResponse";
+import {Loader} from "../../components/common/Loader";
+import {StudentToInterview} from "../../../../headhunter-back/src/types/student";
+import {SingleStudent} from "./SingleStudent";
 
 
 export const HrView = () => {
 
+    const {setModal} = useModal();
+    const {user} = useContext(UserContext);
+    const {apiLoading} = useFetch();
+    const [students, setStudents] = useState<AvailableStudentsResponse[] | StudentToInterview[]>([]);
     const [viewMode, setViewMode] = useState<HrViewMode>(HrViewMode.AvailableStudents);
-    const [students, setStudents] = useState<SampleStudent[]>(sampleStudents);
     const [currentPageNr, setCurrentPageNr] = useState(1);
     const [totalPagesNr, setTotalPagesNr] = useState(0);
     const [maxStudentsPerPage, setMaxStudentsPerPage] = useState(5);
     const [filteringCriteria, setFilteringCriteria] = useState<HrFilteringCriteria | null>(null);
-    const {fetchApi, apiError, data, apiLoading} = useFetch();
-    const { setModal } = useModal();
-    const {user} = useContext(UserContext);
-
 
     const handleFiltering = (data: HrFilteringCriteria) => {
         setFilteringCriteria(data);
@@ -35,7 +36,7 @@ export const HrView = () => {
     const setMaxPerPage = (e: React.ChangeEvent<HTMLSelectElement>) => {
         /* @TODO: useFetch to set max records per page @ backend */
         e.preventDefault();
-        setMaxStudentsPerPage(prev => Number(e.target.value));
+        setMaxStudentsPerPage(Number(e.target.value));
     }
 
     const prevPage = () => {
@@ -50,38 +51,62 @@ export const HrView = () => {
         } else setCurrentPageNr(prev => prev + 1)
     }
 
-    useEffect( () => {
-        // @TODO: actual fetch should be used here to get students and total pages nr. Fetch api route conditionally based on viewmode (different route for viewmode Available Students and different for Students to Interview.
+    useEffect(() => {
+        const fetchData = async (viewMode: HrViewMode) => {
 
-        // if (viewMode === HrViewMode.AvailableStudents) {}
-
-        (async () => {
-            await fetchApi(user, `http://localhost:3000/hr/available`, "GET", "Wystąpił błąd");
-            console.log('oto dane:', data)
-        })();
-
-        console.log('hi')
-
-        const totalStudents = data as AvailableStudentsResponse[]
-        const splitNr = maxStudentsPerPage
-
-        // Temporary function for simulating pagination @ backend. It splits into chunks.
-        const result = totalStudents.reduce((resultArray: any, item: AvailableStudentsResponse, index) => {
-            const chunkIndex = Math.floor(index / splitNr)
-            if (!resultArray[chunkIndex]) {
-                resultArray[chunkIndex] = [] //start a new chunk
+            function paginate(data: Array<any>) {
+                const totalStudents = data
+                const splitNr = maxStudentsPerPage
+                const result = totalStudents.reduce((resultArray: any, item: any, index: number) => {
+                    const chunkIndex = Math.floor(index / splitNr)
+                    if (!resultArray[chunkIndex]) {
+                        resultArray[chunkIndex] = []
+                    }
+                    resultArray[chunkIndex].push(item)
+                    return resultArray
+                }, []);
+                setTotalPagesNr(result.length);
+                return result[currentPageNr - 1];
             }
-            resultArray[chunkIndex].push(item)
-            return resultArray
-        }, []);
-        setTotalPagesNr(result.length);
-        setStudents(result[currentPageNr - 1]);
-    }, [viewMode, maxStudentsPerPage, currentPageNr, filteringCriteria]);
+
+            if (viewMode === HrViewMode.AvailableStudents) {
+                const res = await fetch(`http://localhost:3000/hr/available`, {
+                    method: "GET",
+                    credentials: "include",
+                    headers: {
+                        "Authorization": `Bearer ${user?.access_token}`,
+                    }
+                });
+                const data = await res.json();
+                const paginated = paginate(data.payload as AvailableStudentsResponse[]);
+                setStudents(paginated);
+            }
+
+            if (viewMode === HrViewMode.StudentsToInterview) {
+                const res = await fetch(`http://localhost:3000/hr/interview`, {
+                    method: "GET",
+                    credentials: "include",
+                    headers: {
+                        "Authorization": `Bearer ${user?.access_token}`,
+                    }
+                });
+                const data = await res.json();
+                const paginated = paginate(data.payload as StudentToInterview[]);
+                setStudents(paginated);
+            }
+        };
+
+        void fetchData(viewMode);
+
+
+    }, [viewMode, currentPageNr, maxStudentsPerPage]);
+
+    if (apiLoading) return <Loader/>
 
     return (
         <>
             <div className="flex justify-center items-center w-full mt-4">
-                <div className="flex flex-col place-content-between bg-base-300 items-center w-[1430px]">
+                <div className="flex flex-col place-content-between bg-base-300 items-center max-w-[1430px]">
 
                     <div className="flex flex-row w-full items-start pt-2 border-b-[3px] border-base-200">
 
@@ -103,14 +128,17 @@ export const HrView = () => {
                             </div>
 
                             <button
-                                onClick={() => {setModal(<FilteringModal handleFiltering={handleFiltering}/>)}}
+                                onClick={() => {
+                                    setModal(<FilteringModal handleFiltering={handleFiltering}/>)
+                                }}
                                 className="cursor-pointer w-min-fit px-4 flex items-center justify-center text-neutral-500 bg-base-200 gap-1.5">
                                 <FaFilter className="fill-neutral-500 min-w-[15%] min-h-[15%]"/>Filtrowanie
                             </button>
 
                         </div>
                         <div className="flex flex-col bg-base-200 gap-3">
-                            {students.map((student: SampleStudent, index) =>
+
+                            {students?.map((student: AvailableStudentsResponse | StudentToInterview, index) =>
                                 <SingleStudent
                                     key={index}
                                     viewMode={viewMode}
@@ -127,6 +155,5 @@ export const HrView = () => {
                     </div>
                 </div>
             </div>
-        </>
-    )
+        </>)
 }
