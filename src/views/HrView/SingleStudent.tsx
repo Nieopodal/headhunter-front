6a0793593ae3d7622fc8a06f29b54af0516af061
mp-martin <martin.paper@gmail.com>
2023-05-09 22:27:12 +0200

    types implementation

--- src/views/HrView/SingleStudent.tsx
+++ src/views/HrView/SingleStudent.tsx
@@ -1,19 +1,32 @@
 import React from "react";
-import {SampleStudent} from "../../helpers/sampleStudents";
 import {monthDeclension, numberWithSpaces} from "../../helpers/formatting";
 import {SingleStudentGrade} from "./SingleStudentGrade";
 import {SingleStudentPreferenceItem} from "./SingleStudentPrefernceItem";
 import {HrViewMode} from "../../types/HrViewMode";
 import {SingleStudentTitleBar} from "./SingleStudentTitleBar";
 import {AvailableStudentsResponse} from "../../types/AvailableStudentsResponse";
+import { StudentToInterview } from "../../../../headhunter-back/src/types/student";
 
 
 type Props = {
-    studentData: AvailableStudentsResponse;
+    studentData: AvailableStudentsResponse | StudentToInterview;
     viewMode: HrViewMode
 }
 
-export const SingleStudent = (props: Props) => {
+export const SingleStudent = ({studentData, viewMode}: Props) => {
+
+
+    const courseCompletion = ("student_course_completion" in studentData) ? studentData.student_course_completion : 0;
+    const courseEngagement = ("student_course_engagement" in studentData) ? studentData.student_course_engagement : 0;
+    const projectDegree = ("student_project_degree" in studentData) ? studentData.student_project_degree : 0;
+    const scrumDegree = ("student_team_project_degree" in studentData) ? studentData.student_team_project_degree : 0;
+    const typeWork = ("student_expected_type_work" in studentData) ? studentData.student_expected_type_work : "";
+    const workCity = ("student_target_work_city" in studentData) ? studentData.student_target_work_city : "";
+    const salary = ("student_expected_salary" in studentData) ? studentData.student_expected_salary : 0;
+    const takeAppr = ("student_can_take_apprenticeship" in studentData) ? studentData.student_can_take_apprenticeship : 0;
+    const commExp = ("student_months_of_commercial_exp" in studentData) ? studentData.student_months_of_commercial_exp : 0;
+
+
 
     return (<>
         <div tabIndex={0} className="collapse collapse-arrow rounded-box ">
@@ -21,43 +34,43 @@ export const SingleStudent = (props: Props) => {
 
             <div
                 className="flex flex-row place-content-between items-center collapse-title text-lg font-normal pl-5 pr-0 bg-base-300 after:text-neutral-500 after:scale-150">
-                <SingleStudentTitleBar viewMode={props.viewMode} studentData={props.studentData}/>
+                <SingleStudentTitleBar viewMode={viewMode} studentData={studentData}/>
             </div>
 
             <div
                 className="flex flex-row gap-0.5 bg-base-300 collapse-content p-0 max-lg:overflow-x-scroll max-lg:scroll-mx-96">
                 <SingleStudentGrade
                     title={`Ocena przejścia kursu`}
-                    grade={props.studentData.student_course_completion}
+                    grade={courseCompletion}
                 />
 
                 <SingleStudentGrade
                     title={`Ocena aktywności i zaangażowania na kursie`}
-                    grade={props.studentData.student_course_engagement}/>
+                    grade={courseEngagement}/>
 
                 <SingleStudentGrade
                     title={`Ocena kodu w projekcie własnym`}
-                    grade={props.studentData.student_project_degree}
+                    grade={projectDegree}
                 />
 
                 <SingleStudentGrade
                     title={`Ocena pracy w zespole w Scrum`}
-                    grade={props.studentData.student_team_project_degree}
+                    grade={scrumDegree}
                 />
 
                 <SingleStudentPreferenceItem
                     title={`Preferowane miejsce pracy`}
-                    description={props.studentData.student_expected_type_work}
+                    description={typeWork}
                 />
 
                 <SingleStudentPreferenceItem
                     title={`Docelowe miasto, gdzie chce pracować kandydat`}
-                    description={props.studentData.student_target_work_city}
+                    description={workCity}
                 />
 
                 <SingleStudentPreferenceItem
                     title={`Oczekiwany typ kontraktu`}
-                    description={props.studentData.student_target_work_city} //@TODO: change this to the proper one
+                    description={workCity} //@TODO: change this to the proper one
                 />
 
                 <SingleStudentPreferenceItem
@@ -65,17 +78,17 @@ export const SingleStudent = (props: Props) => {
                     description={numberWithSpaces(Intl.NumberFormat(undefined, {
                         currency: "PLN",
                         style: "currency"
-                    }).format(props.studentData.student_expected_salary))}
+                    }).format(salary))}
                 />
 
                 <SingleStudentPreferenceItem
                     title={`Zgoda na odbycie bezpłatnych praktyk/stażu na początek`}
-                    description={props.studentData.student_can_take_apprenticeship}
+                    description={takeAppr === 1 ? "TAK" : "NIE"}
                 />
 
                 <SingleStudentPreferenceItem
                     title={`Komercyjne doświadczenie w programowaniu`}
-                    description={props.studentData.student_months_of_commercial_exp + ' ' + monthDeclension(props.studentData.student_months_of_commercial_exp)}
+                    description={commExp + ' ' + monthDeclension(commExp)}
                 />
             </div>
         </div>
--- src/views/HrView/SingleStudentTitleBar.tsx
+++ src/views/HrView/SingleStudentTitleBar.tsx
@@ -1,40 +1,53 @@
 import React from "react";
+import { AvailableStudentsResponse } from "src/types/AvailableStudentsResponse";
+import { StudentToInterview } from "../../../../headhunter-back/src/types/student";
 import {HrViewMode} from "../../types/HrViewMode";
-import {SampleStudent} from "../../helpers/sampleStudents";
+
 
 type Props = {
     viewMode: HrViewMode,
-    studentData: SampleStudent,
+    studentData: AvailableStudentsResponse | StudentToInterview,
 }
 
-export const SingleStudentTitleBar = (props: Props) => {
+export const SingleStudentTitleBar = ({studentData, viewMode}: Props) => {
 
-    const userFullName = `${props.studentData.firstName} ` + `${props.studentData.lastName}`;
-    const userShortName = `${props.studentData.firstName} ` + (`${props.studentData.lastName}`).slice(0, 1).concat('.');
+
+    const firstName = ("student_first_name" in studentData) ? studentData.student_first_name : studentData.firstName;
+
+    const lastName = ("student_last_name" in studentData) ? studentData.student_last_name : studentData.lastName;
+
+    const reservationTime = ("reservationTime" in studentData) ? studentData.reservationTime : new Date();
+
+    const pictureUrl = ("pictureUrl" in studentData) ? studentData.pictureUrl : "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
+
+
+
+    const userFullName = `${firstName} ${lastName}`;
+    const userShortName = `${firstName} ${lastName.slice(0, 1).concat('.')}`;
 
     return (<>
         <div className="flex flex-row items-center gap-10">
-            {props.viewMode === HrViewMode.StudentsToInterview ?
+            {viewMode === HrViewMode.StudentsToInterview ?
                 <div className="flex flex-col">
                     <span className="text-sm font-normal">Rezerwacja do:</span>
                     <span
-                        className="text-base font-bold">{new Intl.DateTimeFormat('pl').format(new Date(props.studentData.availableTill))} r.</span>
+                        className="text-base font-bold">{new Intl.DateTimeFormat('pl').format(new Date(reservationTime))} r.</span>
                 </div> : null}
 
             <div className="flex flex-row items-center gap-3">
-                {props.viewMode === HrViewMode.StudentsToInterview ?
+                {viewMode === HrViewMode.StudentsToInterview ?
                     <label className="btn-circle cursor-pointer avatar items-center">
                         <div className="w-20 rounded-full">
-                            <img src={`${props.studentData.pictureUrl}`} alt="user profile"/>
+                            <img src={`${pictureUrl}`} alt="user profile"/>
                         </div>
                     </label> : null}
-                {props.viewMode === HrViewMode.AvailableStudents ? userShortName : userFullName}
+                {viewMode === HrViewMode.AvailableStudents ? userShortName : userFullName}
             </div>
 
         </div>
 
         <div className="flex flex-row items-center gap-3 mr-14">
-            {props.viewMode === HrViewMode.StudentsToInterview ? <>
+            {viewMode === HrViewMode.StudentsToInterview ? <>
                 <button
                     className="z-10 w-1/8 btn-sm h-9 btn-primary normal-case font-normal text-base rounded-none">
                     Pokaż CV
@@ -51,7 +64,7 @@ export const SingleStudentTitleBar = (props: Props) => {
                 </button>
             </> : null}
 
-            {props.viewMode === HrViewMode.AvailableStudents ?
+            {viewMode === HrViewMode.AvailableStudents ?
                 <button
                     className="z-10 w-1/8 btn-sm h-9 btn-primary normal-case font-normal text-base rounded-none">
                     Zarezerwuj rozmowę
