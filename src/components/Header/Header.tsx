3e3ff19f833fc2863e1f81a8fb321b3fde6b1841
mp-martin <martin.paper@gmail.com>
2023-05-10 21:59:43 +0200

    cursor pointer on logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import {AppLogo} from "./AppLogo";
 export const Header = () => (
     <div className="flex justify-center items-center bg-base-200 w-full h-[80px]">
         <div className="flex place-content-between items-center w-[1430px] h-full">
-            <AppLogo classes={"w-24 p-3"}/>
+            <AppLogo classes={"w-24 p-3 cursor-pointer"}/>
             <HeaderMenu/>
         </div>
     </div>
