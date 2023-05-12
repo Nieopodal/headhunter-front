47655d6276c6a2f30ae8a74889d8fa2900413e63
Nieopodal <nieopodal.dev@gmail.com>
2023-05-12 17:59:34 +0200

    Added spinners for checking if user is logged in and login process.

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {FormProvider, useForm} from "react-hook-form";
 import * as yup from 'yup';
 import {useAuth} from "../hooks/useAuth";
@@ -6,14 +6,15 @@ import {AppLogo} from "../components/Header/AppLogo";
 import {Input} from "../components/common/Form/Input";
 import {UserContext} from "../contexts/user.context";
 import {NavLink, useNavigate} from "react-router-dom";
-import {Loader} from "../components/common/Loader";
 import {yupResolver} from "@hookform/resolvers/yup";
 import {ResponseParagraph} from "../components/common/ResponseParagraph";
+import {Loader} from "../components/common/Loader";
 
 export const LoginView = () => {
     const navigate = useNavigate();
     const {user} = useContext(UserContext);
     const {loginUser, findUser, apiLoading, error} = useAuth();
+    const [checkingUser, setCheckingUser] = useState<boolean>(false);
 
     const validationSchema = yup.object({
         email: yup.string().email("Podaj poprawny adres email.").required("Pole jest wymagane."),
@@ -35,46 +36,54 @@ export const LoginView = () => {
             navigate('/dashboard', {replace: true});
         } else {
             (async () => {
+                setCheckingUser(true);
                 await findUser();
+                setCheckingUser(false);
             })();
         }
     }, []);
 
-    if (!user && !apiLoading) return <form onSubmit={methods.handleSubmit(data => loginUser(data))}>
+    return <form onSubmit={methods.handleSubmit(data => loginUser(data))}>
         <FormProvider {...methods}>
             <div className="flex flex-col items-center justify-center h-screen">
-                <div className="w-[350px] flex flex-col">
-                    <div className="flex justify-center">
-                        <AppLogo classes="w-32"/>
-
-                    </div>
-                    <div className="form-control gap-4 my-4">
-                        {error && <div className="w-fit mx-auto"><ResponseParagraph text="Login i/lub hasło są nieprawidłowe."/></div>}
-                        <div>
-                            <Input type="email" name="email" placeholder="E-mail" additionalClasses="block w-full"/>
-                            {errors?.email && <ResponseParagraph text={errors.email.message!}/>}
+                {checkingUser && <Loader/>}
+                {(!checkingUser && !user) &&
+                    <div className="w-[350px] flex flex-col">
+                        <div className="flex justify-center">
+                            <AppLogo classes="w-32"/>
                         </div>
+                        <div className="form-control gap-4 my-4">
+                            {error && <div className="w-fit mx-auto">
+                                <ResponseParagraph text="Login i/lub hasło są nieprawidłowe."/></div>}
+                            <div>
+                                <Input type="email" name="email" placeholder="E-mail" additionalClasses="block w-full"
+                                       disabled={apiLoading}/>
+                                {errors?.email && <ResponseParagraph text={errors.email.message!}/>}
+                            </div>
 
-                        <div>
-                        <Input type="password" name="password" placeholder="Hasło" additionalClasses="block w-full"/>
-                            {errors?.password && <ResponseParagraph text={errors.password.message!}/>}
+                            <div>
+                                <Input type="password" name="password" placeholder="Hasło"
+                                       additionalClasses="block w-full" disabled={apiLoading}/>
+                                {errors?.password && <ResponseParagraph text={errors.password.message!}/>}
+                            </div>
+                        </div>
+                        <div className="items-center flex flex-row place-content-between align-middle mt-2">
+
+                            <NavLink to="/reset-password"
+                                     className="link link-hover font-medium text-sm tracking-wider">
+                                Zapomniałeś hasła?
+                            </NavLink>
+
+                            <button
+                                className={`${apiLoading ? ' btn btn-square loading' : ''} w-1/3 btn-sm h-10 btn-primary disabled:btn-primary normal-case font-normal text-base rounded-none`}
+                                disabled={apiLoading}
+                            >
+                                {!apiLoading && `Zaloguj się`}
+                            </button>
                         </div>
                     </div>
-                    <div className="items-center flex flex-row place-content-between align-middle mt-2">
-
-                        <NavLink to="/reset-password" className="link link-hover font-medium text-sm tracking-wider">
-                            Zapomniałeś hasła?
-                        </NavLink>
-
-                        <button
-                            className="w-1/3 btn-sm h-10 btn-primary normal-case font-normal text-base rounded-none">
-                            Zaloguj się
-                        </button>
-                    </div>
-                </div>
+                }
             </div>
         </FormProvider>
     </form>
-
-    else return <Loader/>
 };
