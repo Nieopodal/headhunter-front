54134043327981d28e161d66a2ddccf429514069
Nieopodal <nieopodal.dev@gmail.com>
2023-06-02 13:18:25 +0200

    Small code refactor.

--- src/components/common/DashboardContainer.tsx
+++ src/components/common/DashboardContainer.tsx
@@ -11,14 +11,14 @@ export const DashboardContainer = ({children}: Props) => {
     const {user} = useContext(UserContext);
 
     if (!user) return <>{children}</>;
-    else
-        return <>
-            <Header/>
-            <div className="flex flex-row justify-center items-center w-full mt-2">
-                <div className="xl:flex block xl:flex-row w-full xl:w-full 2xl:w-[1500px]">
-                    <UserSideMenu/>
-                    <div className="w-full mx-2">{children}</div>
-                </div>
+
+    else return <>
+        <Header/>
+        <div className="flex flex-row justify-center items-center w-full mt-2">
+            <div className="xl:flex block xl:flex-row w-full xl:w-full 2xl:w-[1500px]">
+                <UserSideMenu/>
+                <div className="w-full mx-2">{children}</div>
             </div>
-        </>
+        </div>
+    </>
 };
